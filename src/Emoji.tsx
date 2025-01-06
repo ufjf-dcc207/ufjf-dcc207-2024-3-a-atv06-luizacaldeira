0a3e7d1e5a483f4b4954ad7f9b8b60665e682ce6
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2025-01-06 20:33:29 -0300

    Adiciona estado

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Emoji.css";
 
 const EMOJIS = new Map<string,string>(
@@ -11,6 +12,28 @@ const EMOJIS = new Map<string,string>(
 //EMOJIS.set("happy","😀");
 
 export default function Emoji(){
-    let situacao = "dead";
-return <div className="emoji">{EMOJIS.get(situacao) || "☠️"}</div>
-}
+    const [situacao, setSituacao] = useState("dead");
+
+    function toHappy(){
+        console.log(`to Happy!"${situacao}`);
+        setSituacao("happy");
+        console.log(`to Happy!"${situacao}`);
+    }
+
+    function toDead(){
+        console.log(`to Dead!${situacao}`);
+        setSituacao("dead");
+    }
+
+    return (
+        <div className="emoji">
+            <div className="face">{EMOJIS.get(situacao) || "☠️"}</div>
+            <div className="acoes">
+                <button onClick={toHappy}>Vivo</button>
+                <button onClick={toDead}>Morto</button>
+
+
+            </div>
+        </div>
+    );
+    }
