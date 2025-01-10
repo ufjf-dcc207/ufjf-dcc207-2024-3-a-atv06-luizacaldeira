510d21e48e685c844c99940de85e88196bda7aca
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2025-01-09 22:03:29 -0300

    Adiciona estado

--- src/Atributo.tsx
+++ src/Atributo.tsx
@@ -1,9 +1,25 @@
 //import { useState } from "react";
 
+import { useState } from "react";
+
 export default function Atributo(){
 
+    const [valor, setValor] = useState (0);
+
+    function onMaisClick(){
+        if(valor === 5){
+            setValor(0);
+        }
+        else {
+            setValor(valor+1);
+        }
+    }
+
     return(
-        <div className="atributo">X</div>
+        <div className="atributo">
+            <span>{valor}</span>
+            <button onClick={onMaisClick}>+</button>
+        </div>
 
     );
 }
