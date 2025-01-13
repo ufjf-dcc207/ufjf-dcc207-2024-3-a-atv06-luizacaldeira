a6d20c9eadbd5dd691e3649348256537d7148745
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2025-01-13 20:25:17 -0300

    Adiciona lógica de atualização de saúde

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -20,18 +20,28 @@ export default function Emoji(){
     const [comida, setComida] = useState(1);
 
     function onAlimentar(){
-        setComida(comida === 5 ? comida : comida + 1);
+        setComida(Math.min(comida + 1, 5));
     }
 
     function onHidratar(){
-        setAgua( agua === 5 ? agua : agua + 1);
+        setAgua(Math.min(agua + 1, 5));
     }
     function onLigaDesligaLuz(){
     }
+
     function onCiclo(){
+
+            setComida(Math.max (0, comida - 1));
+            setAgua(Math.max(0, agua -1));
+            setEnergia(Math.max(0, energia -1 ));
+            if(comida === 0)
+                setSaude(prevSaude => Math.max (0, prevSaude - 1));
+            if(agua === 0)
+                setSaude(prevSaude => Math.max (0, prevSaude - 1));
+            if(energia === 0)
+                    setSaude(prevSaude => Math.max (0, prevSaude - 1));
     }
     
-
     return (
         <div className="emoji">
             <div className="face">{EMOJIS.get(situacao) || "☠️"}</div>
@@ -49,4 +59,4 @@ export default function Emoji(){
             </div>
         </div>
     );
-    }
+}
